c5be55b8531eda8beab4f266c472490f948419c0
Ram181999 <141997273+Ram181999@users.noreply.github.com>
2023-09-19 16:23:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here
+let regexp = /\bthe\b|\ban\b|\ba\b/gi;
+
+let arr = [
+  "The Virupaksha Temple", 
+  "a Victoria Memorial", 
+  "an Tajmahal", 
+];
+let articleLessArray = [];
+let mp = {};
+for (let i = 0; i < arr.length; i++) {
+  let articleLessItem = arr[i].replace(regexp, "").trim();
+  articleLessItem = articleLessItem.replace(/  /g, " "); // remove two spaces with a single space
+  articleLessArray.push(articleLessItem);
+  mp[articleLessItem] = arr[i];
+ 
+}
+articleLessArray.sort();
+ for(let i of articleLessArray){
+	 console.log(i);
+ }
+
 
